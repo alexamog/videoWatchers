efd2cf4ceb4ecb8f31466f6ac185bc95d4535889
alexamog <alexamog@outlook.com>
2023-01-30 10:33:34 -0800

    exposed flask

--- fs/app.py
+++ fs/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/', methods=['POST',"GET"])
 def upload_file():
     print("route hit")
-    print(request.json)
+    print(request.json['videoFile'])
     # file = request.json['videoFile']
     # file.save(f"/videos/{request.form['videoName']}")
     return 201
