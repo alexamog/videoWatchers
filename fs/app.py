b36e02aebc5c13f5c1459da68c427d4b2c6c7044
Bryan Xu <xubryan123@gmail.com>
2023-01-30 10:08:26 -0800

    made some changes

--- fs/app.py
+++ fs/app.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def upload_file():
-    file = request.form['videoFile']
-    file.save(f"/videos/{request.form['videoName']}")
+    print(request.json)
+    # file = request.json['videoFile']
+    # file.save(f"/videos/{request.form['videoName']}")
     return 201
 
 if __name__ == "__main__":
