d0638c3354f9201c9c58360328cdfb90e994518a
alexamog <alexamog@outlook.com>
2023-01-30 10:41:47 -0800

    added cors

--- fs/app.py
+++ fs/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=['POST',"GET"])
 def upload_file():
--- react/src/videoUpload/VideoUpload.jsx
+++ react/src/videoUpload/VideoUpload.jsx
@@ -15,7 +15,7 @@ export default function VideoUpload(){
         "videoFile": null
     });
     const setPost = async (data) =>{
-        const res = await axios.post('http://localhost:5000', data)
+        const res = await axios.post('http://10.5.0.7:5000/', data)
         .then(function (response) {
             console.log(response);
           })
