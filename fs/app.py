48ee3a9ec86df12c4f48187271868addfc2e18a2
alexamog <alexamog@outlook.com>
2023-01-30 10:48:58 -0800

    Made change

--- fs/app.py
+++ fs/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from flask_cors import CORS
-
+import json
 
 app = Flask(__name__)
 CORS(app)
@@ -8,10 +8,14 @@ CORS(app)
 @app.route('/', methods=['POST',"GET"])
 def upload_file():
     print("route hit")
-    print(request.json['videoFile'])
+    response = app.response_class(
+        response=json.dumps({"file_location": "c://dev/test"}),
+        status=200,
+        mimetype='application/json'
+    )
     # file = request.json['videoFile']
     # file.save(f"/videos/{request.form['videoName']}")
-    return 201
+    return response
 
 if __name__ == "__main__":
     app.run(port=8080)
