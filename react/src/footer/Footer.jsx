90675aa7821d9c8a2cce17bf96e6f60c232368d7
alexamog <alexamog@outlook.com>
2023-01-29 17:25:01 -0800

    Testing commit

--- react/src/footer/Footer.jsx
+++ react/src/footer/Footer.jsx
@@ -9,7 +9,7 @@ export default function Footer() {
       }}
     >
       <VStack position="absolute" left="0" right="0" bottom="0">
-        <Text>Made with love ❤️ - Group 38 2023 </Text>
+        <Text>Made with love ❤️ - Group 8 2023 </Text>
       </VStack>
     </div>
   );
