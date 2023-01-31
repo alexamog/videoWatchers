d6aa544d1f70b8b9079cd537321ba5afda5951c5
Bryan Xu <xubryan123@gmail.com>
2023-01-30 18:43:11 -0800

    split get and post functionality on fs and changed file path to use os join

--- fs/app.py
+++ fs/app.py
@@ -1,21 +1,31 @@
 from flask import Flask, request
 from flask_cors import CORS
 import json
+import os
+
+UPLOAD_FOLDER = '/videos/'
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/', methods=['POST',"GET"])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/', methods=['POST','GET'])
 def upload_file():
-    print("route hit")
-    response = app.response_class(
-        response=json.dumps({"file_location": "c://dev/test"}),
-        status=200,
-        mimetype='application/json'
-    )
-    # file = request.json['videoFile']
-    # file.save(f"/videos/{request.form['videoName']}")
-    return response
+    if request.method == 'GET':
+        response = app.response_class(
+            response=json.dumps({"file_location": "c://dev/test"}),
+            status=200,
+            mimetype='application/json'
+        )
+
+        return response
+    
+    if request.method == 'POST':
+        file = request.json['videoFile']
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], request.json['videoName']))    
+        
+        return 201
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
