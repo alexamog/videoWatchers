41f97d97b3f30bbca00007dfcb53240e929d822d
alexamog <alexamog@outlook.com>
2023-01-30 20:23:37 -0800

    Implemented video transfer. <3 u bryan

--- react/src/videoUpload/VideoUpload.jsx
+++ react/src/videoUpload/VideoUpload.jsx
@@ -14,23 +14,24 @@ export default function VideoUpload(){
         "tags": [],
         "videoFile": null
     });
-    const setPost = async (data) =>{
-        const res = await axios.post('http://127.0.0.1:5000/', data)
-        .then(function (response) {
+    const setPost = async () =>{
+        const bodyFormData = new FormData();
+        console.log(formData.videoFile);
+        bodyFormData.append("videoName", formData.videoName);
+        bodyFormData.append("tags", formData.tags);
+        bodyFormData.append("videoFile", formData.videoFile);
+        const res = await axios.post("http://localhost:5000/",bodyFormData)
+        .then((response) =>{
             console.log(response);
           })
-          .catch(function (error) {
+          .catch((error) => {
             console.log(error);
           });
     }
     
     const handleClick = async (e) =>{
         e.preventDefault();
-        setPost(formData);
-        // await setPost(formData); //recieve location url
-        // insertVideo()
-        // console.log(formData);
-        
+        setPost();
     }
 
     if(!token){
@@ -50,7 +51,7 @@ export default function VideoUpload(){
             <br/>
             <label>File: </label>
             <input type='file' onChange={(e) =>
-                setFormData({ ...formData, videoFile: e.target.value })
+                setFormData({ ...formData, videoFile: e.target.files[0] })
               }/>
               <br/>
               <label>Tags: </label>
@@ -59,11 +60,7 @@ export default function VideoUpload(){
               }}/>
             <button type="submit" onClick={handleClick}>Submit</button>
             </form>
-            <form action="http://localhost:3001/db/upload" method="post">
-            <input type="email" name="username"  id="exampleInputPassword1" placeholder="Password"/>
-            <button type="submit">Submit</button>
                 
-            </form>
         </div>
     )
 }
