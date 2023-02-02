53445a8800cd51818a2347662cc36ca437068b5a
alexamog <alexamog@outlook.com>
2023-02-01 20:42:44 -0800

    Fixed db, modified docker composer, installed different mysql dependency, added health check

--- db/data.sql
+++ db/data.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE videoapp;
+FLUSH PRIVILEGES;
 USE videoapp;
-
 CREATE TABLE videos(
     video_id INT(11) NOT NULL AUTO_INCREMENT,
     video_title VARCHAR(255) NOT NULL,
@@ -17,8 +17,7 @@ CREATE TABLE videousers(
     user_lastName VARCHAR(100) NOT NULL,
     PRIMARY KEY (User_Id)
 );
-INSERT INTO videousers(user_email, user_username, User_password) VALUES ('aamog@my.bcit.ca','bleulenny','123', 'Alexander','Amog');
-
-CREATE USER 'videouser'@'%' IDENTIFIED BY 'videouser3495';
-GRANT ALL PRIVILEGES ON videoapp.* TO 'videouser'@'%' WITH GRANT OPTION;
+INSERT INTO videousers(user_email, user_username, user_password,user_firstName,user_lastName) VALUES ('aamog@my.bcit.ca','bleulenny','123', 'Alexander','Amog');
+CREATE USER 'videoapp-user'@'%' IDENTIFIED BY 'videouser3495';
+GRANT ALL PRIVILEGES ON videoapp.* TO 'videoapp-user'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
