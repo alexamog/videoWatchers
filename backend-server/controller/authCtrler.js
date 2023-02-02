49a97234764357aeee133d1ec5c62973659062a9
Amanda Kuiack <akuiack@my.bcit.ca>
2023-02-01 17:56:20 -0800

    adjust auth controller add more to db controller

--- backend-server/controller/authCtrler.js
+++ backend-server/controller/authCtrler.js
@@ -1,23 +1,17 @@
-import { db } from "../db.js"
+import { conn } from "../db.js"
 
 export const register = (req,res)=>{
     //check if user exists
     const lookForUser = "SELECT * FROM videousers WHERE user_email = ? OR user_username = ?";
 
-    db.query(lookForUser, [res.body.email], (err,result)=>{
-        if(err) return res.json(err);
-        if(result.length) return res.json("This user already exists.");
+    conn.query(lookForUser, [res.body.email], (err,result)=>{
+        if (err) return res.json(err);
+        if (result.length) return res.json("This user already exists.");
 
         //create user if does not exist
         const createUser = "INSERT INTO videousers(`user_email`, `user_username`, `user_password`, `user_firstName`, `user_lastName`) VALUES (?)"
-        const userValues = [
-            req.body.email,
-            req.body.username,
-            req.body.password,
-            req.body.firstName,
-            req.body.lastName
-        ];
-        db.query(createUser, [userValues], (err, result)=>{
+        const userValues = [req.body.email, req.body.username, req.body.password, req.body.firstName, req.body.lastName];
+        conn.query(createUser, [userValues], (err, result)=>{
                 if (err) return res.json(err);
                 return res.json("User has been created.")
         });
@@ -27,9 +21,10 @@ export const register = (req,res)=>{
 
 export const login = (req,res)=>{
     //check user
-    const getUser = "SELECT * FROM videousers WHERE user_email = ?";
-
-    db.query(getUser, [req.body.email], (err, result) => {
+    const getUser = "SELECT * FROM videousers WHERE user_email = ? AND user_passowrd = ?";
+    const userEmail = req.body.email
+    const userPW = req.body.password
+    conn.query(getUser, [userEmail, userPW], (err, result) => {
         if (err) return res.json(err);
         if (result.length === 0) return res.json("User not found.");
         //check password
--- backend-server/controller/dbCtrler.js
+++ backend-server/controller/dbCtrler.js
@@ -1,7 +1,43 @@
+import conn from "../db";
+
 export const uploadVideo = (req,res)=>{
 
+    //does video with that title already exist?
+    const findVideo = "SELECT * FROM videos WHERE video_title = ? AND video_owner = ?";
+    const vidTitle = req.body.videoName;
+    const vidUser = req.body.username;
+    conn.query(findVideo, [vidTitle,vidUser], (err, result)=>{
+        if (err) return res.json(err);
+        if (result.length !== 0) return res.json("Video by that name already exists from this user.");
+    });
+
+    //upload video
+    const insertVideo = "INSERT INTO videos(`video_title`, `video_Path`, `video_owner`) VALUES (?)";
+    const videoValues = [req.body.videoName, req.body.videoFile, req.body.username];
+    conn.query(insertVideo, [videoValues], (err, result)=>{
+        if (err) return res.json(err);
+
+        return res.json("Video uploaded");
+    });
 };
 
 export const getVideos = (req,res)=>{
+    //pull all videos from database
+    const lookForVideos = "SELECT * FROM videos";
+    conn.query(lookForVideos, (err,result)=>{
+        if (err) return res.json(err);
+        return res.json(result);
+    });
+};
 
+export const getOneVideo = (req, res)=>{
+    //pull video file name from database
+    const findVideo = "SELECT * FROM videos WHERE video_title = ? AND video_owner = ?";
+    const vidTitle = req.body.videoName;
+    const vidUser = req.body.username;
+    conn.query(findVideo, [vidTitle,vidUser], (err, result)=>{
+        if (err) return res.json(err);
+        if (result.length === 0) return res.json("Video no longer exists.");
+        return result[0].video_Path;
+    });
 };
